75f1554a02bf0ae6ca03acd794329961ded58fa3
Rafaela Correa <rafaela.f.c.correa@gmail.com>
2026-09-07 15:48:20 -0300

    Adiciona analytics do Plausible na build de publicacao

--- publishconf.py
+++ publishconf.py
@@ -12,4 +12,8 @@ RELATIVE_URLS = False
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
+# Analytics do Plausible (self-hosted). Fica so aqui de proposito: builds
+# locais com pelicanconf.py nao carregam o script e nao poluem as metricas.
+PLAUSIBLE_SRC = "https://devsnorte-plausible.fly.dev/js/pa-gKUKjoVP2RtbDSVcv6dD0.js"
+
 DELETE_OUTPUT_DIRECTORY = True
